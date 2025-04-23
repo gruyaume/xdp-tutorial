9233d46b26342a8ed740813476677ca7defec5c2
Guillaume Belanger <guillaume.belanger27@gmail.com>
2025-04-23 19:41:38 -0400

    chore: remove unnecessary comments
    Signed-off-by: Guillaume Belanger <guillaume.belanger27@gmail.com>

--- cmd/router/main.go
+++ cmd/router/main.go
@@ -5,7 +5,6 @@ import (
 	"fmt"
 	"log"
 	"net"
-	"time"
 
 	"github.com/gruyaume/xdp-tutorial/internal/config"
 	"github.com/gruyaume/xdp-tutorial/internal/pass"
@@ -81,92 +80,6 @@ func main() {
 			return
 		}
 		log.Printf("updated neighbor %s", neighbor.IP)
-	}
-
-	// r1Idx, err := net.InterfaceByName(ifaceNames[0])
-	// if err != nil {
-	// 	log.Printf("Error getting interface index for %s: %v", ifaceNames[0], err)
-	// 	return
-	// }
-	// r2Idx, err := net.InterfaceByName(ifaceNames[1])
-	// if err != nil {
-	// 	log.Printf("Error getting interface index for %s: %v", ifaceNames[1], err)
-	// 	return
-	// }
-
-	// err = program.Objs.UpdateRoute(&pass.RouteOpts{
-	// 	Prefixlen: 32,
-	// 	Dst:       net.ParseIP("10.1.0.1"),
-	// 	Ifindex:   uint32(r2Idx.Index),
-	// 	Gateway:   net.ParseIP("0.0.0.0"),
-	// })
-	// if err != nil {
-	// 	log.Printf("Error updating route: %v", err)
-	// 	return
-	// }
-
-	// err = program.Objs.UpdateRoute(&pass.RouteOpts{
-	// 	Prefixlen: 32,
-	// 	Dst:       net.ParseIP("10.0.0.1"),
-	// 	Ifindex:   uint32(r1Idx.Index),
-	// 	Gateway:   net.ParseIP("0.0.0.0"),
-	// })
-	// if err != nil {
-	// 	log.Printf("Error updating route: %v", err)
-	// 	return
-	// }
-
-	// err = program.Objs.UpdateInterface(r1Idx)
-	// if err != nil {
-	// 	log.Printf("Error updating interface: %v", err)
-	// 	return
-	// }
-
-	// err = program.Objs.UpdateInterface(r2Idx)
-	// if err != nil {
-	// 	log.Printf("Error updating interface: %v", err)
-	// 	return
-	// }
-	// err = program.Objs.UpdateNeighbor(&pass.NeighborOpts{
-	// 	IP:  "10.0.0.1",
-	// 	MAC: "c6:9f:fb:e6:cc:1f",
-	// })
-	// if err != nil {
-	// 	log.Printf("Error updating neighbor: %v", err)
-	// 	return
-	// }
-	// err = program.Objs.UpdateNeighbor(&pass.NeighborOpts{
-	// 	IP:  "10.1.0.1",
-	// 	MAC: "8a:93:d5:28:9e:35",
-	// })
-	// if err != nil {
-	// 	log.Printf("Error updating neighbor: %v", err)
-	// 	return
-	// }
-
-	ticker := time.NewTicker(3 * time.Second)
-	defer ticker.Stop()
-	for range ticker.C {
-		bytes, err := program.Objs.GetBytesNumber(pass.XDP_PASS)
-		if err != nil {
-			log.Printf("Error getting bytes number (XDP_PASS): %v", err)
-			continue
-		}
-		fmt.Println("Bytes number:", bytes)
-		packets, err := program.Objs.GetPacketsNumber(pass.XDP_PASS)
-		if err != nil {
-			log.Printf("Error getting packets number (XDP_PASS): %v", err)
-			continue
-		}
-		fmt.Println("Packets number:", packets)
-		routes, err := program.Objs.ListRoutes()
-		if err != nil {
-			log.Printf("Error listing routes: %v", err)
-			continue
-		}
-		for _, route := range routes {
-			fmt.Printf("Route: %s/%d via %s\n", route.Dst, route.Prefixlen, route.Gateway)
-		}
 	}
 
 	select {}
