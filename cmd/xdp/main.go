06c8ea98b7ef9a7baef8d3e9f47e1ec2d43dd5aa
Guillaume Belanger <guillaume.belanger27@gmail.com>
2025-04-23 17:05:59 -0400

    chore: validate drop behavior
    Signed-off-by: Guillaume Belanger <guillaume.belanger27@gmail.com>

--- cmd/xdp/main.go
+++ cmd/xdp/main.go
@@ -41,17 +41,17 @@ func main() {
 	log.Printf("Attached XDP program to iface %q (index %d)", iface.Name, iface.Index)
 	log.Printf("Press Ctrl-C to exit and remove the program")
 
-	routeOpts := &pass.RouteOpts{
-		Prefixlen: 32,
-		Dst:       net.ParseIP("10.0.0.254"),
-		Ifindex:   uint32(iface.Index),
-		Gateway:   net.ParseIP("0.0.0.0"),
-	}
-	err = program.Objs.UpdateRoute(routeOpts)
-	if err != nil {
-		log.Printf("Error updating route: %v", err)
-		return
-	}
+	// routeOpts := &pass.RouteOpts{
+	// 	Prefixlen: 32,
+	// 	Dst:       net.ParseIP("10.0.0.254"),
+	// 	Ifindex:   uint32(iface.Index),
+	// 	Gateway:   net.ParseIP("0.0.0.0"),
+	// }
+	// err = program.Objs.UpdateRoute(routeOpts)
+	// if err != nil {
+	// 	log.Printf("Error updating route: %v", err)
+	// 	return
+	// }
 	ticker := time.NewTicker(3 * time.Second)
 	defer ticker.Stop()
 	for range ticker.C {
